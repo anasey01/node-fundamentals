dd2550d7649ca4199338bbb0b53f4f7ecea4152b
Oluseyi <anasey@outlook.com>
2017-09-25 09:31:45 -0400

    // Creating a server and serving HTML to the Client Via Stream (Pipe).

--- app.js
+++ app.js
@@ -1,45 +1,14 @@
+let fs = require('fs');
+let http = require('http');
 
-let stuffs = require('./modules/stuffs')
-let events = require('events')
-let util = require('util');
 
-// //Call the events emitter constructor on the modules.exports on events
-// let eventEmitter = new events.EventEmitter();
-
-// eventEmitter.on('someRandomEvent', function(message){
-//     console.log(message);
-// });
-
-// eventEmitter.emit('someRandomEvent', 'Yeaaa! Event was emitted!! Yipee!');
-
-//One of the things util does is to help us inherit object constructor so that we can attach some custom 
-//events to that object constructor
-
-//creating a object constructor of person
-let Person = function(name){
-this.name = name;
-}
-
-//Now, I want to inherit this object constructor so that a custom event can be attached to it.
-util.inherits(Person, events.EventEmitter); //This inherits the object constructor and the EventEmitter.
-
-//Creating persons
-let eunice = new Person('eunice');
-let oluseyi = new Person('oluseyi');
-let sandra = new Person('sandra');
-
-//Putting persons into an array
-let people = [eunice, oluseyi, sandra];
-//Circling through the array of people.
-people.forEach(function(person){
- //Attact a custom event on each person.
- person.on('speak', function(msg){
-    console.log(person.name + ' said: ' + msg);
- });
+// Creating a server and serving HTML to the Client Via Stream (Pipe).
+let server = http.createServer(function(req, res){
+console.log(req.url);
+res.writeHead(200, {'Content-Type':'text/html'});
+let myReadStream = fs.createReadStream(__dirname + '/index.html');
+myReadStream.pipe(res)
 });
 
-eunice.emit('speak', 'Oh, some JavaScript in my system');
-oluseyi.emit('speak', 'I\'m gonna be slaying me some JS');
-sandra.emit('speak', 'I just wanna have fun');
-
-
+server.listen(3000, '127.0.0.1');
+console.log('Server is listening on port 3000');
